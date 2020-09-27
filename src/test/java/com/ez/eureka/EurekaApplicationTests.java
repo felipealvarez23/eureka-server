d02d330fa591fb50fa859facff67a28ead6834c7
Felipe Alvarez <felipealvarez23@outlook.com>
2020-09-26 19:25:13 -0500

    Version base

---
+++ src/test/java/com/ez/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ez.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
